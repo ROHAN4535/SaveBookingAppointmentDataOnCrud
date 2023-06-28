1f2f1353576972e14e58c7e15196584565a3acf0
ROHAN4535 <grohan468@gmail.com>
2023-06-28 16:15:45 +0530

    Saving the user Details on Crud Crud

---
+++ CrudCrud.js
@@ -0,0 +1,56 @@
+function saveToLocalStorage(event){
+    event.preventDefault();
+    const name = event.target.username.value;
+    const email = event.target.emailid.value;
+
+    const userData = {
+        name,
+        email
+    }
+    
+// ********** Save Data to Cloud using CrudCrud and POSTMAN **********//
+
+    axios.post("https://crudcrud.com/api/52dc9a1a2d98426887299c53510e3b3c/appointmentData",userData)
+        .then((response) => {
+            //console.log(response);
+            showUserOnScreen(response.data);
+        })
+        .catch((err) => {
+            document.body.innerHTML = document.body.innerHTML + "<h4>Ooops! Something Went Wrong.</h4>"
+            console.log(err);
+        })
+
+}
+function showUserOnScreen(userData){
+    const ul = document.getElementById('items');
+    const li = document.createElement('li');
+
+    li.textContent = userData.name + '-' + userData.email;
+    
+    const Delete = document.createElement('input');
+    Delete.type = 'button';
+    Delete.value = 'Delete';
+    
+    Delete.onclick = ()=>{
+        localStorage.removeItem(userData.email);
+        ul.removeChild(li);    
+    }
+    const Edit = document.createElement('input');
+    Edit.type = 'button';
+    Edit.value = 'Edit';
+    
+    Edit.onclick = (event)=>{
+        var lst = event.target.parentElement;
+        let arr = lst.childNodes[0].textContent.split('-');
+        
+        ul.removeChild(li);   
+        document.getElementById('name').value=arr[0];
+        document.getElementById('email').value=arr[1]; 
+        localStorage.removeItem(userData.email);
+    }
+
+    li.appendChild(Delete);
+    li.appendChild(Edit);
+    ul.appendChild(li);
+    
+}
